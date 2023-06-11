3f922547aa5175c58e5124336d292ae38d523ab1
Max DeLiso <me@maxdeliso.name>
2023-06-11 00:02:48 -0400

    start on pairs

--- test/church.test.ts
+++ test/church.test.ts
@@ -1,9 +1,10 @@
 import { parse } from '../lib/parser'
 import { stepMany } from '../lib/evaluator'
-import { Expression, apply } from '../lib/expression'
+import { Expression, apply, prettyPrint } from '../lib/expression'
 
 import { describe, it } from 'mocha'
 import { expect } from 'chai'
+import { I, K, S } from '../lib/terminal'
 
 /*
  * This test verifies that numeral systems and boolean logic can be encoded
@@ -23,6 +24,7 @@ import { expect } from 'chai'
  * λfx.x ≡ KI
  */
 const Zero = parse('KI')
+const Snd = Zero
 
 /*
  * false is the second alternative of two arguments
@@ -41,6 +43,7 @@ const False = Zero
  * λab.a ≡ K ≡ True
  */
 const True = parse('K')
+const Fst = True
 
 /*
  * One. apply a function to its arguments once.
@@ -81,38 +84,138 @@ const B = parse('S(KS)K')
  * (Kf)x(nfx) (by K)
  * f(nfx)
  *
- * λnfx.f(nfx) ≡ S(S(SK)K) ≡ Succ
+ * SBnfx
+ * Bf(nf)x
+ * f((nf)x)
+ * f(nfx)
+ *
+ * λnfx.f(nfx) ≡ S(S(SK)K) ≡ SB ≡ Succ
  *
  * this acts as the successor function for Church numerals
  */
 const Succ = parse('S(S(SK)K)')
 
+const reduce = (exp: Expression): Expression =>
+  stepMany(exp).expr
+
+const ChurchN = (n: number): Expression => {
+  if (n < 0) {
+    throw new Error('only positive integers represented')
+  } else if (n === 0) {
+    return Zero
+  } else if (n === 1) {
+    return One
+  } else {
+    return reduce(apply(Succ, ChurchN(n - 1)))
+  }
+}
+
+const ChurchB = (b: boolean): Expression => b ? True : False
+
+const UpTo = (n: number): Array<number> => {
+  const result = []
+  for (let i = n; i >= 0; i--) {
+    result.push(i)
+  }
+  return result
+}
+
+/*
+ * Cardinal
+ *
+ * or flip once removed
+ *
+ * λxyz.xzy
+ *
+ * S(BBS)(KK)xyz
+ * (BBS)x((KK)x)yz
+ * ((BB)S)xKyz
+ * Sx(Ky)z
+ * xz((Ky)z)
+ * xzy
+ *
+ * λxyz.xzy ≡ S(BBS)(KK)
+ */
+const C = apply(S, apply(B, B, S), apply(K, K))
+
+/*
+ * Thrush
+ *
+ * or flip
+ *
+ * λxy.yx
+ *
+ * CIxy
+ * Iyx
+ * yx
+ *
+ * λxy.yx ≡ CI ≡ flip
+ */
+const T = apply(C, I)
+
+/*
+ * Vireo
+ *
+ * or Pair
+ *
+ * λabf.fab
+ *
+ * BCTabf
+ * C(Ta)bf
+ * (Ta)fb
+ * fab
+ *
+ * λabf.fab ≡ BCT ≡ Pair
+ *
+ * Pair a b = <a, b>, a 2-tuple waiting for a function.
+ *
+ * Sometimes called a cons cell.
+ */
+const V = apply(B, C, T)
+
+/*
+ * Mockingbird
+ *
+ * λa.aa
+ *
+ * SIIx
+ * Ix(Ix)
+ * x(Ix)
+ * xx
+ *
+ * λa.aa ≡ M
+ */
+const M = parse('SII')
+
+/*
+ * Retrieve the first element in a Cons cell.
+ *
+ * Car p
+ *
+ * p = <a,b>
+ *
+ * T Fst p
+ * p Fst
+ * p K
+ * a
+ */
+const Car = apply(T, Fst)
+
+/*
+ * Retrieve the second element in a Cons cell.
+ *
+ * Cdr p
+ *
+ * p = <a,b>
+ *
+ * T Snd p
+ * p Snd
+ * p KI
+ * b
+ */
+const Cdr = apply(T, Snd)
+
 describe('Church encodings', () => {
-  const reduce = (exp: Expression): Expression =>
-    stepMany(exp).expr
-
-  const ChurchN = (n: number): Expression => {
-    if (n < 0) {
-      throw new Error('only positive integers represented')
-    } else if (n === 0) {
-      return Zero
-    } else if (n === 1) {
-      return One
-    } else {
-      return reduce(apply(Succ, ChurchN(n - 1)))
-    }
-  }
-
-  const ChurchB = (b: boolean): Expression => b ? True : False
-
-  const UpTo = (n: number): Array<number> => {
-    const result = []
-    for (let i = n; i >= 0; i--) {
-      result.push(i)
-    }
-    return result
-  }
-
   it('reduces 0 + 1 to 1 ', () => {
     expect(One)
       .to.deep.equal(ChurchN(1))
@@ -183,5 +286,26 @@ describe('Church encodings', () => {
           .to.deep.equal(dis)
       })
     })
+  })
+
+  it('reduces pairs', () => {
+    console.log(prettyPrint(V))
+    console.log(prettyPrint(M))
+
+    expect(reduce(apply(V, ChurchN(0), ChurchN(1), Fst)))
+      .to.deep.equal(ChurchN(0))
+
+    expect(reduce(apply(V, ChurchN(0), ChurchN(1), Snd)))
+      .to.deep.equal(ChurchN(1))
+
+    expect(
+      reduce(
+        apply(Car, apply(V, ChurchN(0), ChurchN(1)))
+      )).to.deep.equal(ChurchN(0))
+
+    expect(
+      reduce(
+        apply(Cdr, apply(V, ChurchN(0), ChurchN(1)))
+      )).to.deep.equal(ChurchN(1))
   })
 })

a073720b3cb4ef37b72019c4c5e2312a8af83c77
Max DeLiso <me@maxdeliso.name>
2023-12-05 20:36:24 -0500

    add type inferencer, new version, upgrades

--- lib/lambda.ts
+++ lib/lambda.ts
@@ -1,3 +1,5 @@
+import { NonTerminal } from './nonterminal'
+
 /**
  * This is a single term variable with a name.
  *
@@ -12,3 +14,40 @@ export const mkVar = (name: string): LambdaVar => ({
   kind: 'lambda-var',
   name
 })
+
+// λx.<body>, where x is a name
+type UntypedLambdaAbs = {
+  kind: 'lambda-abs',
+  name: string,
+  // eslint-disable-next-line no-use-before-define
+  body: UntypedLambda
+}
+
+export const mkUntypedAbs =
+  // eslint-disable-next-line no-use-before-define
+  (name: string, body: UntypedLambda): UntypedLambda => ({
+    kind: 'lambda-abs',
+    name,
+    body
+  })
+
+/**
+ * The legal terms of the untyped lambda calculus.
+ * e ::= x | λx.e | e e, where x is a variable name, and e is a valid expr
+ */
+export type UntypedLambda
+  = LambdaVar
+  | UntypedLambdaAbs
+  | NonTerminal<UntypedLambda>
+
+export const prettyPrintUntypedLambda = (ut: UntypedLambda): string => {
+  switch (ut.kind) {
+    case 'lambda-var':
+      return ut.name
+    case 'lambda-abs':
+      return `λ${ut.name}.${prettyPrintUntypedLambda(ut.body)}`
+    case 'non-terminal':
+      return `(${prettyPrintUntypedLambda(ut.lft)}` +
+        `${prettyPrintUntypedLambda(ut.rgt)})`
+  }
+}
--- test/types.test.ts
+++ test/types.test.ts
@@ -1,23 +1,108 @@
-import { arrow, arrows, mkTypeVar, typesEqual } from '../lib/types'
+import {
+  arrow,
+  arrows,
+  mkTypeVar,
+  typesLitEq,
+  inferType
+} from '../lib/types'
 
 import { expect } from 'chai'
 import { describe, it } from 'mocha'
+import { UntypedLambda, mkUntypedAbs, mkVar } from '../lib/lambda'
+import { nt } from '../lib/nonterminal'
+import { typedTermsLitEq } from '../lib/typedLambda'
+import { parseType, parseTypedLambda } from '../lib'
 
-describe('Type construction', () => {
+describe('type construction and equivalence', () => {
   const t1 = arrows(mkTypeVar('a'), mkTypeVar('b'), mkTypeVar('c'))
   const t2 = arrows(mkTypeVar('a'), mkTypeVar('b'), mkTypeVar('d'))
+  const t3 = arrows(mkTypeVar('d'), mkTypeVar('e'), mkTypeVar('f'))
 
-  it('recursively checks for type equivalence', () => {
-    expect(typesEqual(t1, t1)).to.equal(true)
-    expect(typesEqual(t1, t2)).to.equal(false)
+  it('recursively checks for literal type equivalence', () => {
+    expect(typesLitEq(t1, t1)).to.equal(true)
+    expect(typesLitEq(t1, t2)).to.equal(false)
+    expect(typesLitEq(t1, t3)).to.equal(false)
+    expect(typesLitEq(t2, t3)).to.equal(false)
+    expect(typesLitEq(t1, t3)).to.equal(false)
   })
 
   it('associates type construction to the right', () => {
     expect(
-      typesEqual(
+      typesLitEq(
         arrows(mkTypeVar('a'), mkTypeVar('b'), mkTypeVar('c')),
         arrow(mkTypeVar('a'), arrow(mkTypeVar('b'), mkTypeVar('c')))
       )
     ).to.equal(true)
+  })
+})
+
+describe('type inference', () => {
+  it('infers the type of the I combinator', () => {
+    const [termI, typeofI] = inferType(mkUntypedAbs('x', mkVar('x')))
+    const [, parsedTypedI] = parseTypedLambda('λx:a.x')
+    const [, parsedIType] = parseType('a→a')
+
+    expect(typedTermsLitEq(termI, parsedTypedI)).to.equal(true)
+    expect(typesLitEq(typeofI, parsedIType)).to.equal(true)
+  })
+
+  it('infers the type of the K combinator', () => {
+    const [termK, typeofK] = inferType(
+      mkUntypedAbs('x',
+        mkUntypedAbs('y',
+          mkVar('x')))
+    )
+    const [, parsedTypedK] = parseTypedLambda('λx:a.λy:b.x')
+    const [, parsedKType] = parseType('a→b→a')
+
+    expect(typedTermsLitEq(termK, parsedTypedK)).to.equal(true)
+    expect(typesLitEq(typeofK, parsedKType)).to.equal(true)
+  })
+
+  it('infers the type of the S combinator', () => {
+    const [termS, typeofS] = inferType(
+      mkUntypedAbs('x',
+        mkUntypedAbs('y',
+          mkUntypedAbs('z',
+            nt(
+              nt(mkVar('x'), mkVar('z')),
+              nt(mkVar('y'), mkVar('z')))
+          )
+        )
+      ))
+
+    const [, parsedTypedS] = parseTypedLambda('λx:a→b→c.λy:a→b.λz:a.xz(yz)')
+    const [, parsedSType] = parseType('(a→b→c)→(a→b)→a→c')
+
+    expect(typedTermsLitEq(termS, parsedTypedS)).to.equal(true)
+    expect(typesLitEq(typeofS, parsedSType)).to.equal(true)
+  })
+
+  it('succeeds at inferring the type of λx.λy.xy', () => {
+    const [termT, typeofT] = inferType(
+      mkUntypedAbs('x', mkUntypedAbs('y', nt(mkVar('x'), mkVar('y'))))
+    )
+
+    const [, parsedTypedT] = parseTypedLambda('λx:a→b.λy:a.xy')
+    const [, parsedTType] = parseType('(a→b)→(a→b)')
+
+    expect(typedTermsLitEq(termT, parsedTypedT)).to.equal(true)
+    expect(typesLitEq(typeofT, parsedTType)).to.equal(true)
+  })
+
+  it('fails to infer type of λx.xx', () => {
+    expect(() => inferType(mkUntypedAbs('x', nt(mkVar('x'), mkVar('x')))))
+      .to.throw(/type mismatch/)
+  })
+
+  it('fails at inferring the type λx.λy.(xy)x', () => {
+    expect(() => inferType(mkUntypedAbs('x',
+      mkUntypedAbs('y',
+        nt<UntypedLambda>(
+          nt<UntypedLambda>(
+            mkVar('x'),
+            mkVar('y')),
+          mkVar('x')))))
+    ).to.throw(/type mismatch/)
   })
 })

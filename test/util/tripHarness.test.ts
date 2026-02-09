07096858ee9ace73edbc5727363d8d37f0c360b4
Max DeLiso <me@maxdeliso.name>
2026-02-09 17:56:38 -0500

    move the test files to disk

--- test/util/tripHarness.test.ts
+++ test/util/tripHarness.test.ts
@@ -1,18 +1,15 @@
 import { expect } from "chai";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 import { evaluateTrip } from "./tripHarness.ts";
 import { UnChurchNumber } from "../../lib/ski/church.ts";
+import { loadInput } from "./fileLoader.ts";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 Deno.test("TripHarness", async (t) => {
   await t.step("includeNat flag allows using Nat module", async () => {
-    // This program uses the 'succ' function which is exported by the Nat module
-    const source = `
-      module Main
-      import Nat succ
-      import Nat zero
-      export main
-
-      poly main = succ (succ zero)
-    `;
+    const source = loadInput("includeNat.trip", __dirname);
 
     const result = await evaluateTrip(source, { includeNat: true });
     const number = UnChurchNumber(result);

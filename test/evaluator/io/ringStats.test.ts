3b16e511beb8e1f6a0d3c986eb739fec3da2216d
Max DeLiso <me@maxdeliso.name>
2026-04-06 18:40:48 -0400

    fmt

--- test/evaluator/io/ringStats.test.ts
+++ test/evaluator/io/ringStats.test.ts
@@ -29,7 +29,7 @@ test("RingStats - statistics recording", () => {
     duplicateLostAllocs: 2,
     hashconsHits: 30,
     hashconsMisses: 20,
-    totalLinkChaseHops: 0
+    totalLinkChaseHops: 0,
   };
   const snapshot = stats.getSnapshot(5, 10, extra);
 
@@ -64,7 +64,7 @@ test("RingStats - reset", () => {
     duplicateLostAllocs: 0,
     hashconsHits: 0,
     hashconsMisses: 0,
-    totalLinkChaseHops: 0
+    totalLinkChaseHops: 0,
   };
   const snapshot = stats.getSnapshot(0, 0, extra);
   assert.strictEqual(snapshot.submitOk, 0);
